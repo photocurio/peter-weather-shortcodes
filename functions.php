cc2723d13ce8ce736b2a244ee4ba83ea40f1d08a
photocurio <sunraydust@gmail.com>
2023-02-02 09:43:51 -0500

    formatted code per WordPress standard.

--- functions.php
+++ functions.php
@@ -4,82 +4,116 @@ declare(strict_types=1);
 
 /**
  * Map degrees to cardinal direction
+ *
+ * @param int $deg A compass bearing 0-365
  */
-function degrees_to_directional(int $deg): string
-{
-	if (!is_numeric($deg)) return '';
-	else if ($deg < 11) return 'N';
-	else if ($deg < 34) return 'NNE';
-	else if ($deg < 56) return 'NE';
-	else if ($deg < 79) return 'ENE';
-	else if ($deg < 101) return 'E';
-	else if ($deg < 124) return 'ESE';
-	else if ($deg < 146) return 'SE';
-	else if ($deg < 169) return 'SSE';
-	else if ($deg < 191) return 'S';
-	else if ($deg < 214) return 'SSW';
-	else if ($deg < 236) return 'SW';
-	else if ($deg < 259) return 'WSW';
-	else if ($deg < 281) return 'W';
-	else if ($deg < 304) return 'WNW';
-	else if ($deg < 326) return 'NW';
-	else if ($deg < 349) return 'NNW';
-	else return 'N';
+function degrees_to_directional( int $deg ): string {
+	if ( ! is_numeric( $deg ) ) {
+		return '';
+	} elseif ( $deg < 11 ) {
+		return 'N';
+	} elseif ( $deg < 34 ) {
+		return 'NNE';
+	} elseif ( $deg < 56 ) {
+		return 'NE';
+	} elseif ( $deg < 79 ) {
+		return 'ENE';
+	} elseif ( $deg < 101 ) {
+		return 'E';
+	} elseif ( $deg < 124 ) {
+		return 'ESE';
+	} elseif ( $deg < 146 ) {
+		return 'SE';
+	} elseif ( $deg < 169 ) {
+		return 'SSE';
+	} elseif ( $deg < 191 ) {
+		return 'S';
+	} elseif ( $deg < 214 ) {
+		return 'SSW';
+	} elseif ( $deg < 236 ) {
+		return 'SW';
+	} elseif ( $deg < 259 ) {
+		return 'WSW';
+	} elseif ( $deg < 281 ) {
+		return 'W';
+	} elseif ( $deg < 304 ) {
+		return 'WNW';
+	} elseif ( $deg < 326 ) {
+		return 'NW';
+	} elseif ( $deg < 349 ) {
+		return 'NNW';
+	} else {
+		return 'N';
+	}
 }
 
 /**
  * Map weather condition codes to 2 digit weaather icon code
  */
-function find_icon(int $weatherCode): string
-{
-	if (!is_numeric($weatherCode)) return '01';
-	else if ($weatherCode === 511) return '13';
-	else if ($weatherCode === 800) return '01';
-	else if ($weatherCode < 299) 	return '11';
-	else if ($weatherCode < 522)	return '09';
-	else if ($weatherCode < 599) 	return '10';
-	else if ($weatherCode < 699) 	return '13';
-	else if ($weatherCode < 799) 	return '50';
-	else if ($weatherCode < 803) 	return '02';
-	else return '05';
+function find_icon( int $weather_code ): string {
+	if ( ! is_numeric( $weather_code ) ) {
+		return '01';
+	} elseif ( 511 === $weather_code ) {
+		return '13';
+	} elseif ( 800 === $weather_code ) {
+		return '01';
+	} elseif ( $weather_code < 299 ) {
+		return '11';
+	} elseif ( $weather_code < 522 ) {
+		return '09';
+	} elseif ( $weather_code < 599 ) {
+		return '10';
+	} elseif ( $weather_code < 699 ) {
+		return '13';
+	} elseif ( $weather_code < 799 ) {
+		return '50';
+	} elseif ( $weather_code < 803 ) {
+		return '02';
+	} else {
+		return '05';
+	}
 }
 
 /**
  * API Request Caching
  *
- * Use server-side caching to store API requests 
+ * Use server-side caching to store API requests
  * rather than request for each page view.
- * 
+ *
  * @arg Argument description and usage info
  */
-function json_cached_api_results(string $cache_file = NULL, int $expires = NULL, array $params): object
-{
-	if (!$cache_file) $cache_file = dirname(__FILE__) . '/api-cache.json';
-	if (!$expires) $expires = time() - 180; // 3 minutes
+function json_cached_api_results( string $cache_file = null, int $expires = null, array $params ): object {
+	if ( ! $cache_file ) {
+		$cache_file = dirname( __FILE__ ) . '/api-cache.json';
+	}
+	if ( ! $expires ) {
+		$expires = time() - 180; // 3 minutes.
+	}
 
-	if (!file_exists($cache_file)) die("Cache file is missing: $cache_file");
+	if ( ! file_exists( $cache_file ) ) {
+		die( esc_html( "Cache file is missing: $cache_file" ) );
+	}
 
-	// Check that the file is older than the expire time and that it's not empty
-	if (filectime($cache_file) < $expires || file_get_contents($cache_file)  == '') {
+	// Check that the file is older than the expire time and that it's not empty.
+	if ( filectime( $cache_file ) < $expires || file_get_contents( $cache_file ) === '' ) {
+		// File is too old, refresh cache.
+		$url         = 'https://api.openweathermap.org/data/3.0/onecall?units=imperial&lat=';
+		$url        .= $params['lat'] . '&lon=' . $params['lon'] . '&appid=' . $params['appid'] . '&exclude=minutely,hourly';
+		$api_results = wp_remote_get( $url );
+		$json_data   = $api_results['body'];
 
-		// File is too old, refresh cache
-		$url  = 'https://api.openweathermap.org/data/3.0/onecall?units=imperial&lat=';
-		$url .= $params['lat'] . '&lon=' . $params['lon'] . '&appid=' . $params['appid'] . '&exclude=minutely,hourly';
-		$api_results = wp_remote_get($url);
-		$json_data = $api_results['body'];
-
-		// Remove cache file on error to avoid writing bad data
-		if ($api_results && isset($api_results['body'])) {
-			file_put_contents($cache_file, $json_data);
-			return json_decode($json_data);
+		// Remove cache file on error to avoid writing bad data.
+		if ( $api_results && isset( $api_results['body'] ) ) {
+			file_put_contents( $cache_file, $json_data );
+			return json_decode( $json_data );
 		} else {
-			unlink($cache_file);
+			unlink( $cache_file );
 		}
 		return 'There was an error getting the weather data';
 	} else {
-		// Fetch cache
-		$json_results = file_get_contents($cache_file);
+		// Fetch cache.
+		$json_results = file_get_contents( $cache_file );
 	}
 
-	return json_decode($json_results);
+	return json_decode( $json_results );
 }
--- peter-weather-shortcodes.php
+++ peter-weather-shortcodes.php
@@ -1,10 +1,13 @@
 <?php
-/*
-Plugin Name: Peter's Weather Shortcodes
-Description: Show weather forcast in a sidebar widget. Also displays warning on top of the Posts feed.
-Version: 0.30
-Author: Peter Mumford
-*/
+
+/**
+ * Plugin Name: Peter's Weather Shortcodes
+ * Description: Show weather forcast in a sidebar widget. Also displays warning on top of the Posts feed.
+ * Version: 0.30
+ * Author: Peter Mumford
+ *
+ * @package peter-weather-shortcodes
+ */
 
 declare(strict_types=1);
 
@@ -12,45 +15,47 @@ require 'functions.php';
 
 class PeterWeatherShortcodes
 {
-	public function __construct()
-	{
-		add_action('wp_enqueue_scripts', array($this, 'weather_add_stylesheet'));
-		add_shortcode('weather-shortcode', array($this, 'weather_shortcode_func'));
-		add_shortcode('small-craft-advisory', array($this, 'weather_sca_func'));
+	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'weather_add_stylesheet' ) );
+		add_shortcode( 'weather-shortcode', array( $this, 'weather_shortcode_func' ) );
+		add_shortcode( 'small-craft-advisory', array( $this, 'weather_sca_func' ) );
 	}
 
-	/**  
+	/**
 	 * Add stylesheet to the page
 	 */
-	public function weather_add_stylesheet(): void
-	{
-		wp_enqueue_style('prefix-style', plugins_url('css/styles.css', __FILE__), false, '0.30');
+	public function weather_add_stylesheet(): void {
+		wp_enqueue_style( 'prefix-style', plugins_url( 'css/styles.css', __FILE__ ), false, '0.30' );
 	}
 
 	/**
 	 * The output for the weather shortcode.
 	 */
-	public function weather_shortcode_func(array $atts): string
-	{
-		$a = shortcode_atts(array(
-			'lon' => '',
-			'lat' => '',
-			'appid' => '',
-			'locationname' => ''
-		), $atts);
+	public function weather_shortcode_func( array $atts ): string {
+		$a = shortcode_atts(
+			array(
+				'lon'          => '',
+				'lat'          => '',
+				'appid'        => '',
+				'locationname' => '',
+			),
+			$atts
+		);
 
 		/**
 		 * Check for empty attributes.
 		 */
-		$nullValue = false;
-		foreach ($a as $key => $value) {
-			if (empty($value)) {
-				$nullValue = true;
+		$null_value = false;
+		foreach ( $a as $key => $value ) {
+			if ( empty( $value ) ) {
+				$null_value = true;
 				break;
 			}
 		}
-		if ($nullValue) return 'Add lat, lon, OpenWeather API key, and location name to weather shortcode';
-		$weatherJson = json_cached_api_results(NULL, 300, $a);
+		if ( $null_value ) {
+			return 'Add lat, lon, OpenWeather API key, and location name to weather shortcode';
+		}
+		$weather_json = json_cached_api_results( null, 300, $a );
 		/**
 		 * Using an output buffer to assemble the markup doesn't work in a class.
 		 * We have to concatenate a string.
@@ -58,69 +63,72 @@ class PeterWeatherShortcodes
 		$output  = '<div class="peter-weather-widget">';
 		$output .= '<h3 class="weather-title">Current weather at ' . $a['locationname'] . '</h3>';
 		$output .= '<p class="weather-period">updated every 5 minutes</p>';
-		$output .= '<img src="' . plugin_dir_url(__FILE__) . 'icons/' . esc_attr(find_icon($weatherJson->current->weather[0]->id)) . '.png" class="weather-icon current" />';
+		$output .= '<img src="' . plugin_dir_url( __FILE__ ) . 'icons/';
+		$output .= esc_attr( find_icon( $weather_json->current->weather[0]->id ) );
+		$output .= '.png" class="weather-icon current" />';
 		$output .= '<div>';
 		$output .= '<div class="flex-table"><span class="flex-row header">TEMP</span><span class="flex-row day">';
-		$output .= esc_html(round($weatherJson->current->temp)) . '&deg;F</span></div>';
+		$output .= esc_html( round( $weather_json->current->temp ) ) . '&deg;F</span></div>';
 		$output .= '<div class="flex-table"><span class="flex-row header">WEATHER</span><span class="flex-row day">';
-		$output .= esc_html($weatherJson->current->weather[0]->description) . '</span></div>';
+		$output .= esc_html( $weather_json->current->weather[0]->description ) . '</span></div>';
 		$output .= '<div class="flex-table"><span class="flex-row header">WIND</span>';
-		$output .= '<span class="flex-row day">' . esc_html(round($weatherJson->current->wind_speed)) . ' MPH, ';
-		$output .= esc_html(degrees_to_directional($weatherJson->current->wind_deg)) . '</span></div></div><hr>';
+		$output .= '<span class="flex-row day">' . esc_html( round( $weather_json->current->wind_speed ) ) . ' MPH, ';
+		$output .= esc_html( degrees_to_directional( $weather_json->current->wind_deg ) ) . '</span></div></div><hr>';
 
-		foreach ($weatherJson->daily as $day) {
+		foreach ( $weather_json->daily as $day ) {
 			$output .= '<div class="day">';
-			$output .= '<img src="' . plugin_dir_url(__FILE__) . 'icons/' . esc_attr(find_icon($day->weather[0]->id)) . '.png" class="weather-icon" />';
-			$output .= '<h5 class="day-heading">' . date('l', $day->dt) . ' forecast</h5>';
+			$output .= '<img src="' . plugin_dir_url( __FILE__ ) . 'icons/' . esc_attr( find_icon( $day->weather[0]->id ) );
+			$output .= '.png" class="weather-icon" />';
+			$output .= '<h5 class="day-heading">' . date( 'l', $day->dt ) . ' forecast</h5>';
 			$output .= '<div class="flex-table"><span class="flex-row header">TEMP</span>';
 			$output .= '<span class="flex-row day">';
-			$output .= esc_html(round($day->temp->day)) . '&deg;F';
+			$output .= esc_html( round( $day->temp->day ) ) . '&deg;F';
 			$output .= '</span></div>';
 			$output .= '<div class="flex-table"><span class="flex-row header">WEATHER</span>';
-			$output .= '<span class="flex-row day">' . esc_html($day->weather[0]->description) . '</span>';
+			$output .= '<span class="flex-row day">' . esc_html( $day->weather[0]->description ) . '</span>';
 			$output .= '</div><div class="flex-table"><span class="flex-row header">WIND</span>';
-			$output .= '<span class="flex-row day">' . esc_html(round($day->wind_speed)) . 'MPH, ';
-			$output .= esc_html(degrees_to_directional($day->wind_deg));
+			$output .= '<span class="flex-row day">' . esc_html( round( $day->wind_speed ) ) . 'MPH, ';
+			$output .= esc_html( degrees_to_directional( $day->wind_deg ) );
 			$output .= '</span></div><hr></div>';
-		} // end foreach loop 
-		$output .= '<p class="weather-update">last updated ' . wp_date('j F Y g:i A', $weatherJson->current->dt) . '</p>';
+		} // end foreach loop
+		$output .= '<p class="weather-update">last updated ';
+		$output .= wp_date( 'j F Y g:i A', $weather_json->current->dt ) . '</p>';
 		$output .= '</div>';
 
 		return $output;
 	}
 
-	public function weather_sca_func(): string
-	{
-		$cache_file = dirname(__FILE__) . '/api-cache.json';
-		if (!file_exists($cache_file)) {
-			return "";
+	public function weather_sca_func(): string {
+		$cache_file = dirname( __FILE__ ) . '/api-cache.json';
+		if ( ! file_exists( $cache_file ) ) {
+			return '';
 		}
 
-		$results = file_get_contents($cache_file);
-		$json_results = json_decode($results);
+		$results      = file_get_contents( $cache_file );
+		$json_results = json_decode( $results );
 
-		if (!isset($json_results->alerts)) {
-			return "";
+		if ( ! isset( $json_results->alerts ) ) {
+			return '';
 		}
 
-		foreach ($json_results->alerts as $alert) {
+		foreach ( $json_results->alerts as $alert ) {
 			if (
-				str_contains($alert->event, 'Small Craft') ||
-				str_contains($alert->event, 'Gale') ||
-				str_contains($alert->event, 'Storm') ||
-				str_contains($alert->event, 'Hurricane') ||
-				str_contains($alert->event, 'Dense Fog') ||
-				str_contains($alert->event, 'Thunderstorm')
+				str_contains( $alert->event, 'Small Craft' ) ||
+				str_contains( $alert->event, 'Gale' ) ||
+				str_contains( $alert->event, 'Storm' ) ||
+				str_contains( $alert->event, 'Hurricane' ) ||
+				str_contains( $alert->event, 'Dense Fog' ) ||
+				str_contains( $alert->event, 'Thunderstorm' )
 			) {
 				/**
 				 * Use regex to format the Advisory with paragraphs and list items.
 				 */
-				$description = preg_replace('/\n/', ' ', $alert->description);
-				$description = preg_replace('/\*/', '</li><li>', $description);
+				$description = preg_replace( '/\n/', ' ', $alert->description );
+				$description = preg_replace( '/\*/', '</li><li>', $description );
 
 				$output  = '<article class="post-entry warning">';
-				$output .= '<h2 class="post-title warning">' . esc_html($alert->event) . '</h2>';
-				$output .= '<ul><li>' . wp_kses_post($description) . '</li></ul>';
+				$output .= '<h2 class="post-title warning">' . esc_html( $alert->event ) . '</h2>';
+				$output .= '<ul><li>' . wp_kses_post( $description ) . '</li></ul>';
 				$output .= '</article>';
 			} else {
 				$output = '';
@@ -130,4 +138,4 @@ class PeterWeatherShortcodes
 	}
 }
 
-$peterWeatherShortcodes = new PeterWeatherShortcodes();
+$peter_weather_shortcodes = new PeterWeatherShortcodes();
